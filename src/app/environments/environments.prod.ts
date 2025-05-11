b12abf605ad0a4547ec325062ca56308a85bae11
Jebimeta <jebilongo@gmail.com>
2025-05-11 17:38:08 +0200

    Environments added

---
+++ src/app/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseURL: 'https://puntillismoshop.com/api/v1'
+}
---
+++ src/app/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environments = {
+  baseUrl: 'http://localhost:8080/api/v1',
+  baseUsersUrl:'http://localhost:8080/api/v1/users',
+  baseAuthUrl: 'http://localhost:8080/api/v1/auth',
+  baseProductsURL: 'http://localhost:8080/api/v1/shop',
+  baseCartURL: 'http://localhost:8080/api/v1/shop/cart',
+  baseContactUsUrl: 'http://localhost:8080/api/v1/about/send-email',
+}
